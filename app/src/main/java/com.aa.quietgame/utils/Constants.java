bd576e8fe121ba744bbe12ffc792cabdfea4638e
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-16 01:55:24 -0500

    Create Constants.java

---
+++ app/src/main/java/com.aa.quietgame/utils/Constants.java
@@ -0,0 +1,22 @@
+public class Constants {
+
+    // Constants for Intent extras
+    public static final String EXTRA_PLAYER_NAME = "player_name";
+    public static final String EXTRA_PLAYER_AVATAR = "player_avatar";
+    public static final String EXTRA_PLAYER_SCORE = "player_score";
+
+    // Constants for SharedPreferences keys
+    public static final String PREFS_KEY_PLAYER_NAME = "player_name";
+    public static final String PREFS_KEY_PLAYER_AVATAR = "player_avatar";
+    public static final String PREFS_KEY_PLAYER_HIGH_SCORE = "player_high_score";
+
+    // Constants for avatar selection
+    public static final int AVATAR_DEFAULT = 0;
+    public static final int AVATAR_CAT = 1;
+    public static final int AVATAR_DOG = 2;
+    public static final int AVATAR_MONKEY = 3;
+    public static final int AVATAR_PENGUIN = 4;
+    public static final int AVATAR_RABBIT = 5;
+    public static final int AVATAR_ROBOT = 6;
+
+}
