a30cf1409e18c9ca9dda717cab346ea9c3638f91
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-16 02:16:01 -0500

    Create ProfileActivity.java

---
+++ app/src/main/java/com.aa.quietgame/activities/ProfileActivity.java
@@ -0,0 +1,10 @@
+# Outline ProfileActivity.java for the Quiet Game app.
+
+The ProfileActivity is responsible for displaying the player's profile information and allowing them to edit it. Here are some possible features that could be included in the ProfileActivity:
+
+- Display player information: Display the player's name, avatar, and other profile information.
+- Edit player information: Allow the player to edit their name, avatar, and other profile information.
+  player profile: Provide an option for the player to delete their profile from the app.
+- View player statistics: Display the player's game statistics, such as their highest score and longest time played.
+- Share player profile: Provide an option for the player to share their profile with friends via social media.
+- The ProfileActivity will need to be linked to the rest of the app so that any changes made by the player are reflected in the game. Additionally, the ProfileActivity would need to integrate with the ProfileService to store and retrieve player information.
